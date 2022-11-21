718c8993f8ba179120daea832636af16f1333c75
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-21 09:26:17 +0530

    Update 2.Void_pointer.c

--- 2.Void_pointer.c
+++ 2.Void_pointer.c
@@ -10,7 +10,7 @@ void main()
     *(int*)p=12;
   
 
-    printf("%d\n",num);//printing cell address of variable 
+    printf("%d\n",num);//printing value of variable 
     /* void pointer holds the address of any datatype such as int , float, char and by assigning the address we can type cast it. The *p points the value stored at the address held by p . the next value assigned to the pointer is 12 and the same is displayed.*/
     
 }
