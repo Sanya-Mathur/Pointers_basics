454f409c4f7db3a3860e2acfa0fbd80642bf3031
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-21 13:50:54 +0530

    Create 5.Addition_with_pointers.c

---
+++ 5.Addition_with_pointers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+printf("RA2211042010042\n");
+int a =5,b=7,c,d,*ap,*bp;
+ap=&a;
+bp=&b;
+d=*ap+*bp;
+printf("sum of a & b=%d\n",c);
+printf("sum of a & b=%d\n",d);
+}
+
+    
