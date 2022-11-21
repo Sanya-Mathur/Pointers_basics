c2ed9448a784a9e626f47251269b72ffcc17682e
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-21 09:31:14 +0530

    Create 4.Memory_requirements.c

---
+++ 4.Memory_requirements.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+void main()
+{ 
+    printf("RA2211042010042\n");
+    printf("char %d byte and its pointer %d bytes\n",sizeof(char),sizeof(char*));
+  printf("int %d byte and its pointer %d bytes\n",sizeof(int),sizeof(int*));
+  printf("float %d byte and its pointer %d bytes\n",sizeof(float),sizeof(float*));
+}
