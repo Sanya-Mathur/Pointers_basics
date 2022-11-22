0807a3344b1932fabf0b02539c843ed28b1f1c59
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-22 10:40:56 +0530

    Update 11.Decrementing_a_pointer.c

--- 11.Decrementing_a_pointer.c
+++ 11.Decrementing_a_pointer.c
@@ -1,12 +1,11 @@
 
 #include <stdio.h>
-const int max = 3;
 int main ()
 { printf("RA2211042010042\n");
  int ar[] = {10, 100, 200};
  int i, *ptr;
- ptr =&ar[max-1];
- for ( i = max; i>0; i--)
+ ptr =&ar[2];
+ for ( i = 3; i>0; i--)
  {
  printf("Address of array[%d] = %x\n", i, ptr );
  printf("Value of array[%d] = %d\n", i, *ptr );
