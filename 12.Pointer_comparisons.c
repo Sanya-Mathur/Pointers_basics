482287cfbd226e502211c9439578d3746bbd130d
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-22 10:39:46 +0530

    Update 12.Pointer_comparisons.c

--- 12.Pointer_comparisons.c
+++ 12.Pointer_comparisons.c
@@ -1,16 +1,15 @@
 
 #include <stdio.h>
-const int max = 3;
 int main ()
 { printf("RA2211042010042\n");
  int ar[] = {10, 100, 200};
  int i, *ptr;
  ptr = ar;
  i = 0;
- while ( ptr <= &ar[max - 1] )
+ while ( ptr <= &ar[2] )
  {
- printf("Address of var[%d] = %x\n", i, ptr );
- printf("Value of var[%d] = %d\n", i, *ptr );
+ printf("Address of array[%d] = %x\n", i, ptr );
+ printf("Value of array[%d] = %d\n", i, *ptr );
  //point to the previous location 
  ptr++;
  i++;
