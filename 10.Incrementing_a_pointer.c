662c348fb8949d3196d0279f219d56987bd0c376
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-22 10:41:24 +0530

    Update 10.Incrementing_a_pointer.c

--- 10.Incrementing_a_pointer.c
+++ 10.Incrementing_a_pointer.c
@@ -1,12 +1,11 @@
 
 #include <stdio.h>
-const int max = 3;
 int main ()
 { printf("RA2211042010042\n");
  int ar[] = {10, 100, 200};
  int i, *ptr;
  ptr = ar;
- for ( i = 0; i < max; i++)
+ for ( i = 0; i < 3; i++)
  {
  printf("Address of array[%d] = %x\n", i, ptr );
  printf("Value of array[%d] = %d\n", i, *ptr );
